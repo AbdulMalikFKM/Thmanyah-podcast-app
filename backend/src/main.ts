28f27d6e0949941d2874797b34de02f2be378a13
AbdulMalikFKM <AbdulAlmalik2121@gmail.com>
2026-01-21 00:13:39 +0300

    fixing app listen

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,14 +4,19 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Enable CORS
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  }); //(Cross-Origin Resource Sharing)
+  });
 
-  await app.listen(process.env.PORT ?? 3001, '0.0.0.0'); //Chnage from 3000 to 3001 since next.js use 3000
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  const port = process.env.PORT || 3001;
+
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`Server is officially live on port: ${port}`);
 }
+
 bootstrap().catch((err) => {
   console.error('Error starting server:', err);
   process.exit(1);
