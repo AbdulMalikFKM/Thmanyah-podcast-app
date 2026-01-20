fcda537b3fc5e53bc2b7f1df0c3b919a0b6723ff
AbdulMalikFKM <AbdulAlmalik2121@gmail.com>
2026-01-21 00:08:29 +0300

    fixing CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors(); //(Cross-Origin Resource Sharing)
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  }); //(Cross-Origin Resource Sharing)
 
-  await app.listen(process.env.PORT ?? 3001); //Chnage from 3000 to 3001 since next.js use 3000
+  await app.listen(process.env.PORT ?? 3001, '0.0.0.0'); //Chnage from 3000 to 3001 since next.js use 3000
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap().catch((err) => {
   console.error('Error starting server:', err);
